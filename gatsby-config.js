d5041f51b2bd49a67ffa9c94cdd5beea041d0932
Andreas Gies <andreas@wayofquality.de>
2022-06-18 15:03:36 +0200

    Clean up after migrating gatsby node to theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,20 +16,18 @@ const sitePlugins = ([
   {
     resolve: `@atooni/gatsby-theme-ziosite`
   },
-  // Use the postcss plugin to configure tailwind as a post processor
   {
-    resolve: `gatsby-plugin-postcss`,
-    options: {
-      postCssPlugins: [
-        require(`tailwindcss`)
-      ]
+    resolve: '@chakra-ui/gatsby-plugin',
+    options: { 
+      resetCSS: true,
+      isUsingColorMode: true
     }
-  },  
+  }
   // Configure the file system plugin for generated docs
 ]).concat(
-  includeProject('zmx', 'zio-metrics-connectors')
-  //includeProject('zio1', 'zio-core'),
-  //includeProject('zio2', 'zio-core-2')
+  includeProject('zmx', 'zio-metrics-connectors'),
+  includeProject('zio1', 'zio-core'),
+  includeProject('zio2', 'zio-core-2')
 )
 
 const config = { 
